e33fc64ed6b9838d1f9b1a1210fd300811cb1545
Raiza Santana <raiza.santanna@gmail.com>
2015-07-20 16:44:55 -0300

    Finalizando o editar metrica

--- src/servlet/MetricaServlet.java
+++ src/servlet/MetricaServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,7 +21,7 @@ private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 	{
 		String acao = (String) request.getParameter("acao");
-		System.out.println(acao);
+		
 		
 		if (acao != null)
 		{
@@ -29,8 +30,14 @@ private static final long serialVersionUID = 1L;
 				case "Criar":
 					criarMetrica(request, response);
 					break;
-				case "listarCurso":
-					//listarCursosDepartamentos(request, response);
+				case "listarMetricas":
+					listarMetricas(request, response);
+					break;
+				case "editarMetrica":
+					getMetrica(request, response);
+					break;
+				case "atualizarMetrica":
+					editarMetrica(request, response);
 					break;
 				default:
 					break;
@@ -41,9 +48,34 @@ private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
+		String acao = (String) request.getParameter("acao");
 		
+		
+		if (acao != null)
+		{
+			switch (acao)
+			{
+				case "Criar":
+					criarMetrica(request, response);
+					break;
+				case "listarMetricas":
+					listarMetricas(request, response);
+					break;
+				case "editarMetrica":
+					getMetrica(request, response);
+					break;
+				case "atualizarMetrica":
+					editarMetrica(request, response);
+					break;	
+				default:
+					break;
+			}
+		}
+
 	}
 	
+	
+//TODO Adicionar relação com o indicador 
 	private void criarMetrica(HttpServletRequest request,
 			HttpServletResponse response) {
 		String message = "Metrica inserida com sucesso.";
@@ -78,6 +110,75 @@ private static final long serialVersionUID = 1L;
 
 	}
 	
+	
+	private void listarMetricas(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+				
+		MetricaDAO mDAO = new MetricaDAO();
+		ArrayList<Metrica> ms =  mDAO.listarMetricas();
+		
+		request.setAttribute("metricas",ms);
+		
+		try {
+			request.getRequestDispatcher("/metrica/listarMetricas.jsp").forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void getMetrica(HttpServletRequest request,
+			HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		int id = Integer.valueOf(request.getParameter("id"));
+		
+		MetricaDAO mDAO = new MetricaDAO();
+		Metrica m;
+		try {
+			m = mDAO.getMetrica(id);
+			request.setAttribute("metrica_id", m.getId());
+			request.setAttribute("metrica_nome",m.getNome());
+			request.setAttribute("metrica_tipo",m.getTipo());
+			request.setAttribute("metrica_processoColeta",m.getProcessoColeta());
+			request.setAttribute("metrica_processoAnalise",m.getProcessoAnalise());
+			
+			request.getRequestDispatcher("editarMetrica.jsp").forward(request, response);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	public void editarMetrica(HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		
+		int id = Integer.valueOf(request.getParameter("id"));
+
+		
+		Metrica m = new Metrica();
+		m.setId(id);
+		m.setNome(request.getParameter("nome"));
+		m.setProcessoColeta(request.getParameter("processoColeta"));
+		m.setProcessoAnalise(request.getParameter("processoAnalise"));
+		m.setTipo(request.getParameter("tipoMetrica"));
+		
+		MetricaDAO mDAO = new MetricaDAO();
+		try {
+			mDAO.editarMetrica(m);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+		}
+		
+		listarMetricas(request, response);
+		
+	}
+
 	
 
 }
